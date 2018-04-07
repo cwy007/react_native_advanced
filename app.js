b4dd929d487bb4022c27a6d11fbe186c37e94481
cwy007 <chanweiyan007@gmail.com>
2018-04-07 17:16:16 +0800

    learning tabbarios

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import {
 // import LoginScreen from './app/login';
 import FlexLayout from './app/flex_layout';
 import NavigatorTest from './app/welcome/navigator_test';
+import TabbarTest from './app/welcome/tabbar_test';
 
 const AppStart = () => {
-  AppRegistry.registerComponent("react_native_advanced", () => { return NavigatorTest })
+  AppRegistry.registerComponent("react_native_advanced", () => { return TabbarTest })
 }
 
 module.exports = AppStart;
