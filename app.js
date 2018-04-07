b78846b97499a53c8a221a3cd68ee13d355fa68d
cwy007 <chanweiyan007@gmail.com>
2018-04-07 16:56:38 +0800

    intro of navigator component

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import {
 } from 'react-native';
 // import LoginScreen from './app/login';
 import FlexLayout from './app/flex_layout';
+import NavigatorTest from './app/welcome/navigator_test';
 
 const AppStart = () => {
-  AppRegistry.registerComponent("react_native_advanced", () => { return FlexLayout })
+  AppRegistry.registerComponent("react_native_advanced", () => { return NavigatorTest })
 }
 
 module.exports = AppStart;
