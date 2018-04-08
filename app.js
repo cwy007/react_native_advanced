0a91a3e88a3c26c6c424b8d60220d68f9738f95c
cwy007 <chanweiyan007@gmail.com>
2018-04-08 21:35:50 +0800

    using AsyncStorage

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // 先注入
+import {
+  AsyncStorage,
+} from 'react-native';
+import { HelperMemo } from './app/helper';
+
 import { Navigation } from 'react-native-navigation';
 import LoginScreen from './app/login';
 import FlexLayout from './app/flex_layout';
@@ -19,45 +24,63 @@ Navigation.registerComponent('WebViewTest', () => WebViewTest);
 Navigation.registerComponent('AnimatedTest', () => AnimatedTest);
 Navigation.registerComponent('OthersTest', () => OthersTest);
 
+const LoadSavedDataFromDevice = (callback) => {
+  AsyncStorage.getItem('user_data', (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      if (result !== null) {
+        HelperMemo['user_data'] = JSON.parse(result);
+      } else {
+        HelperMemo['user_data'] = null;
+      }
+
+      callback();
+    }
+  })
+}
+
 const AppStart = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        label: "首页",
-        screen: 'LoginScreen',
-        icon: require('./img/home.png'),
-        selectedIcon: require('./img/home_selected.png'),
-        title: '登录',
-      },
-      {
-        label: "商品",
-        screen: 'ProductsScreen',
-        icon: require('./img/home.png'),
-        selectedIcon: require('./img/home_selected.png'),
-        title: '商品',
-      },
-      {
-          label: "WebView",
-          screen: 'WebViewTest',
-          icon: require('./img/safari.png'),
-          selectedIcon: require('./img/safari_selected.png'),
-          title: "WebView",
+  LoadSavedDataFromDevice(() => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          label: "首页",
+          screen: 'LoginScreen',
+          icon: require('./img/home.png'),
+          selectedIcon: require('./img/home_selected.png'),
+          title: '登录',
         },
-      {
-        label: "Animated",
-        screen: 'AnimatedTest',
-        icon: require('./img/user.png'),
-        selectedIcon: require('./img/user_selected.png'),
-        title: 'Animated',
-      },
-      {
-        label: "Others",
-        screen: 'OthersTest',
-        icon: require('./img/box.png'),
-        selectedIcon: require('./img/box_selected.png'),
-        title: "Others",
-      },
-    ]
+        {
+          label: "商品",
+          screen: 'ProductsScreen',
+          icon: require('./img/home.png'),
+          selectedIcon: require('./img/home_selected.png'),
+          title: '商品',
+        },
+        {
+            label: "WebView",
+            screen: 'WebViewTest',
+            icon: require('./img/safari.png'),
+            selectedIcon: require('./img/safari_selected.png'),
+            title: "WebView",
+          },
+        {
+          label: "Animated",
+          screen: 'AnimatedTest',
+          icon: require('./img/user.png'),
+          selectedIcon: require('./img/user_selected.png'),
+          title: 'Animated',
+        },
+        {
+          label: "Others",
+          screen: 'OthersTest',
+          icon: require('./img/box.png'),
+          selectedIcon: require('./img/box_selected.png'),
+          title: "Others",
+        },
+      ]
+    })
   })
 }
 
