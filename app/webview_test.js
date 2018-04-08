1201c455db0da6992aba832e54bde54de2d5373f
cwy007 <chanweiyan007@gmail.com>
2018-04-08 14:23:48 +0800

    webview event processing & click intercept

--- app/webview_test.js
+++ app/webview_test.js
@@ -7,15 +7,46 @@ import {
 
 export default class WebViewTest extends Component {
   render() {
+    var url = this.props.url || "https://eggman.tv";
+
     return (
       <View style={{flex: 1}}>
         <WebView
           style={{flex: 1}}
           ref="webview"
-          source={{html: "<h1>web view</h1>"}}
+          source={{uri: url}}
           contentContainerStyle={{padding: 15,}}
+          onLoadStart={this._onLoadStart.bind(this)}
+          onLoadEnd={this._onLoadEnd.bind(this)}
+          onShouldStartLoadWithRequest={this._onShouldStartLoadWithRequest.bind(this)} //
+          onNavigationStateChange={this._onNavigationStateChange.bind(this)}
         />
       </View>
     )
+  }
+
+  _onShouldStartLoadWithRequest(navState) {
+    if (navState.navigationType == 'click') {
+      this.props.navigator.push({
+        title: "Detail",
+        screen: "WebViewTest",
+        passProps: { url: navState.url }
+      });
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  _onNavigationStateChange(navState) {
+    console.log(navState);
+  }
+
+  _onLoadStart() {
+
+  }
+
+  _onLoadEnd() {
+
   }
 }
